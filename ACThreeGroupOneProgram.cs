4bd3393392ed1e2c81fa6fe268e4650bae66916f
Eudald Fajula <eudald.fajula.7e6@itb.cat>
2024-11-05 12:56:56 +0100

    Update ACThreeGroupOneProgram.cs

--- ACThreeGroupOneProgram.cs
+++ ACThreeGroupOneProgram.cs
@@ -1,51 +1,102 @@
 using System;
-namespace Grup1{
-    public class Super4{
-        public static int NameSelect(int namepj, ref int vocal){
+namespace Grup1
+{
+    public class Super4
+    {
+        public static int NameSelect(string namepj, ref int vocal)
+        {
             int i = 0;
-            for (i = 0; i < namepj; i++){
-                if (namepj[i] == "A" || "E" || "I" || "O" || "U" || "a" || "e" || "i" || "o" || "u"){
+            char[] nameChar = namepj.ToCharArray();
+            for (i = 0; i < namepj.Length; i++)
+            {
+                if (nameChar[i] == 'A' )
+                {
+                    vocal = vocal + 1;
+                }
+                else if (nameChar[i] == 'E')
+                {
+                    vocal = vocal + 1;
+                }
+                else if (nameChar[i] == 'I')
+                {
+                    vocal = vocal + 1;
+                }
+                else if (nameChar[i] == 'O')
+                {
+
+                    vocal = vocal + 1;
+                }
+                else if (nameChar[i] == 'U')
+                {
+                    vocal = vocal + 1;
+                }
+                else if (nameChar[i] == 'a')
+                {
+                    vocal = vocal + 1;
+                }
+                else if (nameChar[i] == 'e')
+                {
+                    vocal = vocal + 1;
+                }
+                else if (nameChar[i] == 'i')
+                {
+                    vocal = vocal + 1;
+                }
+                else if (nameChar[i] == 'o')
+                {
+                    vocal = vocal + 1;
+                }
+                else if (nameChar[i] == 'u')
+                {
                     vocal = vocal + 1;
                 }
             }
             return i;
         }
 
-        static void vocales(int vocal, ref double fuerzasuper4, ref int maldadpj){
-            if (vocal >= 2){
+        static void vocales(int vocal, ref double fuerzasuper4, ref int maldadpj)
+        {
+            if (vocal >= 2)
+            {
                 fuerzasuper4 = maldadpj / 4.0; // Calcular la fuerza como un double
-                maldadpj = maldadpj % 4; 
-            }else{
+                maldadpj = maldadpj % 4;
+            }
+            else
+            {
                 fuerzasuper4 = maldadpj * 0.05; // Fuerza basada en el 5% de maldad
             }
         }
 
-        public static void Main(){
+        public static void Main()
+        {
             const string PJSELECT = "Select your character (1-4):";
             const string NAMESELECT = "Select your name:";
             const string PJMALDADSELECT = "Select your evilness level:";
-            
+
             int numberpj;
             string namepj;
             int maldadpj;
             int vocal = 0;
             double fuerzasuper4 = 0;
 
-            do{
+            do
+            {
                 Console.WriteLine(PJSELECT);
                 numberpj = int.Parse(Console.ReadLine());
-            }while(numberpj > 4 || numberpj < 1); //error arreglat en la condicio del while en el que la segona condicio del or no estaba ben especificada
+            } while (numberpj > 4 || numberpj < 1); //error arreglat en la condicio del while en el que la segona condicio del or no estaba ben especificada
 
-            do{
+            do
+            {
                 Console.WriteLine(NAMESELECT);
-                namepj=Console.ReadLine();
-            }while (nameselect(namepj) > 25 || nameselect(namepj) < 2); //error arreglat en la condicio del while en el que la segona condicio del or no estaba ben especificada
+                namepj = Console.ReadLine();
+            } while (NameSelect(namepj, ref vocal) > 25 || NameSelect(namepj, ref vocal) < 2); //error arreglat en la condicio del while en el que la segona condicio del or no estaba ben especificada
 
-            do{
+            do
+            {
                 Console.WriteLine(PJMALDADSELECT);
                 maldadpj = int.Parse(Console.ReadLine());
-            }while(maldad > 50000 || maldad < 1000); //error arreglat en la condicio del while en el que la segona condicio del or no estaba ben especificada
-            vocales(vocal,ref fuerzasuper4, ref maldad);
+            } while (maldadpj > 50000 || maldadpj < 1000); //error arreglat en la condicio del while en el que la segona condicio del or no estaba ben especificada
+            vocales(vocal, ref fuerzasuper4, ref maldadpj);
         }
     }
 }
